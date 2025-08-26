6ab91bf72e0d82869443a456e173dc35fecd2bcf
Nalor91 <cornwej@eou.edu>
2025-08-26 16:50:39 -0700

    selectionSort Commit

--- SortingExercises/sorting.js
+++ SortingExercises/sorting.js
@@ -118,3 +118,33 @@ function insertionSort(arr) {
 
 // insertionSort(nums); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34,
 //                      //  34, 35, 43, 67, 75, 232, 232, 453, 546, 4342]
+
+function selectionSort(arr) {
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+    for (let i = 0; i < arr.length; i++) {
+        let lowestNum = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[lowestNum]) {
+                lowestNum = j;
+            }
+        }
+        swap (arr, lowestNum, i);
+    }
+
+    return arr;
+}
+
+// selectionSort([4, 20, 12, 10, 7, 9]); // [4, 7, 9, 10, 12, 20]
+// selectionSort([0, -10, 7, 4]); // [-10, 0, 4, 7]
+// selectionSort([1, 2, 3]); // [1, 2, 3]
+// selectionSort([]);
+
+// let nums = [
+//     4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2,
+//     453, 546, 75, 67, 4342, 32
+// ];
+
+// selectionSort(nums); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34,
+//                      //  35, 43, 67, 75, 232, 232, 453, 546, 4342]
