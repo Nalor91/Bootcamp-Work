d4d84863e736b6d9ad16460bdadb15328bc49335
Nalor91 <cornwej@eou.edu>
2025-08-26 17:25:43 -0700

    quickSort commit

--- SortingExercises/sorting.js
+++ SortingExercises/sorting.js
@@ -148,3 +148,30 @@ function selectionSort(arr) {
 
 // selectionSort(nums); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34,
 //                      //  35, 43, 67, 75, 232, 232, 453, 546, 4342]
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+    let pivot = arr[start];
+    let swapIdx = start;
+
+    for (let i = start + 1; i <= end; i++) {
+        if (pivot > arr[i]) {
+        swapIdx++;
+        swap(arr, swapIdx, i);
+        }
+    }
+     swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
