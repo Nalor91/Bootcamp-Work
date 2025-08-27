b71efe6ecba83af88d42c86d07304db0c9106c26
Nalor91 <cornwej@eou.edu>
2025-08-26 17:41:25 -0700

    radixSort Commit

--- SortingExercises/sorting.js
+++ SortingExercises/sorting.js
@@ -175,3 +175,33 @@ function quickSort(arr, left = 0, right = arr.length - 1) {
   }
   return arr;
 }
+
+function getDigit(num, i) {
+    return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}
+
+function digitCount(num) {
+    return String(num).length;
+}
+
+function mostDigits(nums) {
+    let maxDigits = 0;
+    for (let i = 0; i < nums.length; i++) {
+        maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+    }
+    return maxDigits;
+}
+
+function radixSort(nums) {
+    let maxDigitCount = mostDigits(nums);
+    for(let k = 0; k < maxDigitCount; k++) {
+        let buckets = Array.from({length: 10 }, () => []);
+        for (let i = 0; i < nums.length; i++) {
+            let num = nums[i];
+            let digit = getDigit(num, k);
+            buckets[digit].push(num);
+        }
+        nums = [].concat(...buckets);
+    }
+    return nums;
+}
