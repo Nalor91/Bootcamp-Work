fefb372b38f05eabc3adf16598a15fe4a573876a
Nalor91 <cornwej@eou.edu>
2025-09-07 08:35:48 -0700

    Arrows commit

---
+++ Variables and Functions/Variables and Functions/hoisting.js
@@ -0,0 +1,18 @@
+// Task 4
+console.log(timeMachineModel); // Returns an undefined output due to hoisting. It is declared but not assigned until after the console.log command
+
+// Task 1
+let destination = "Ancient Egypt";
+console.log(destination);
+
+// Task 2
+destination = "Medieval Europe";
+console.log(destination);
+
+// Task 3
+const travelDate = "2024-03-15";
+
+// When trying to change the travelDate you get a typeError of assignment to constant variable and are unable to change it.
+
+// Task 4
+var timeMachineModel = "T-800";
