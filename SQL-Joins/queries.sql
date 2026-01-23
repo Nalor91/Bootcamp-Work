80c06cbaf3159d97c760e9922400efe9b290fa1b
Nalor91 <cornwej@eou.edu>
2026-01-23 12:13:17 -0800

    SQL-Joins commit

---
+++ SQL-Joins/queries.sql
@@ -0,0 +1,26 @@
+-- First prompt
+
+SELECT * FROM owners o
+     FULL OUTER JOIN vehicles v
+        ON o.id = v.owner_id;
+
+-- Second prompt
+
+SELECT first_name, last_name,
+  COUNT  (owner_id) FROM owners o
+    JOIN vehicles v
+        ON o.id = v.owner_id
+   GROUP BY (first_name, last_name)
+ORDER BY first_name;
+
+-- Third prompt
+
+SELECT first_name, last_name, 
+  ROUND(AVG(price)) AS average_price,
+  COUNT (owner_id)
+FROM owners o
+    JOIN vehicles v
+        ON o.id = v.owner_id
+   GROUP BY (first_name, last_name)
+   HAVING COUNT(owner_id) > 1 AND ROUND(AVG(price)) > 10000
+ORDER BY first_name DESC;
